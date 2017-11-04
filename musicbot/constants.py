d4f47fe0ce1813061b525820d6e6ec187d4ca1e3
Helixrelic45 <jellco49@gmail.com>
2017-11-04 13:57:22 -0400

    Version Info was updated

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
-MAIN_VERSION = '\nCodeName: Oxygen-3\nPublic Release\nThe October Update'
-SUB_VERSION = ' \nBuild 1.5.1_1'
+MAIN_VERSION = '\nCodeName: Paradox\nPublic Release'
+SUB_VERSION = ' \nBuild 2.1.1'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
